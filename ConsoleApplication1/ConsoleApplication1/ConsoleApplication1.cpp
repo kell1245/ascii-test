4c9ce67c5c0a135e87d20895330c834ebbb91286
kell1245 <3314154247@qq.com>
2025-01-10 15:01:35 +0800

    A-Z的ASCI码练习

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,7 @@
 ﻿#include<iostream>;
 using namespace std;
 int main() {
-	for (int aa = 97;aa <= 122;aa++) {
+	for (int aa = 65;aa <= 90;aa++) {
 		cout << aa << "的ASCII码是：" << char(aa) << endl;
 	}
 	system("pause");
