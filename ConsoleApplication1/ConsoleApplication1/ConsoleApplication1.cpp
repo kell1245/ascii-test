9f9360835aa462a9bbe83fcd05a6719dfdee6d4f
kell1245 <3314154247@qq.com>
2025-01-10 14:55:08 +0800

    0到9的ASCII码练习

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,1 +1,9 @@
-﻿
+﻿#include<iostream>;
+using namespace std;
+int main() {
+	for (int aa = 0;aa <= 9;aa++) {
+		cout << aa << "的ASCII码是：" << char(aa) << endl;
+	}
+	system("pause");
+	return 0;
+    }
