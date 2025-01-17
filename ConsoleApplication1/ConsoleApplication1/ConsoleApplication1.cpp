5d54801f91425f8e56abd5290e827c9c4d66af07
kell1245 <3314154247@qq.com>
2025-01-17 11:07:34 +0800

    唐诗

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include<iostream>;
-using namespace std;
-int main() {
-	for (int aa = 97;aa <= 122;aa++) {
-		cout << aa << "的ASCII码是：" << char(aa) << endl;
-	}
-	system("pause");
-	return 0;
-    }
